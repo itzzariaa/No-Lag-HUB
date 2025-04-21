22e775fe1a67b982dfad85966639bcc4bca6d4d7
NoLag <153074411+NoLag-id@users.noreply.github.com>
2025-04-21 17:13:57 +0700

    Update LoaderV1.lua

--- Loader/LoaderV1.lua
+++ Loader/LoaderV1.lua
@@ -1,7 +1,7 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
 
 local scripts = {
-    [126884695634066] = "https://raw.githubusercontent.com/dawid-scripts/Fluent/refs/heads/master/Example.lua",
+    [126884695634066] = "https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Garden/V0.lua",
     --[3434126884695634066] = "https://raw.githubusercontent.com/dawid-scripts/Fluent/refs/heads/master/Example.lua",
 }
 
