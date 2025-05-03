0408a56f17ded993d6b080e738309a56a2e72e26
DeopZa <87126706+DeopZa@users.noreply.github.com>
2025-05-03 09:25:55 +0700

    Create LoaderV2.lua

---
+++ Loader/LoaderV2.lua
@@ -0,0 +1,11 @@
+repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
+
+local scripts = {
+    [126884695634066] = "https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Garden/Garden-V1.lua",
+    [81440632616906] = "https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/DigEarth/V1.lua",
+}
+
+local url = scripts[game.PlaceId]
+if url then
+    loadstring(game:HttpGetAsync(url))()
+end
