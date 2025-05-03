3c1eac3ce309ed7e23be7c309e905d6421fe2a6a
DeopZa <87126706+DeopZa@users.noreply.github.com>
2025-05-03 09:21:34 +0700

    Update loaderTest.lua

--- loaderTest.lua
+++ loaderTest.lua
@@ -1,5 +1,4 @@
 repeat wait() until game:IsLoaded() and game.Players.LocalPlayer
-local loadNoLag = "https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/untitled.lua"
 local scripts = {
     [126884695634066] = "https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/Garden/Garden-V1.lua",
     [81440632616906] = "https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/DigEarth/V1.lua",
@@ -7,6 +6,6 @@ local scripts = {
 
 local url = scripts[game.PlaceId]
 if url then
-	  loadstring(game:HttpGetAsync(loadNoLag))()
+    loadstring(game:HttpGetAsync("https://raw.githubusercontent.com/NoLag-id/No-Lag-HUB/refs/heads/main/untitled.lua"))()
     loadstring(game:HttpGetAsync(url))()
 end
